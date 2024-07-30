da76b263ea9f1ed9b10502e010fd4c4a2e9d58bf
Cristian Alfonsin <cristiancfack133@gmail.com>
2024-07-30 10:42:30 -0600

    ADD 30/07/2024: Some changes

--- src/components/admin/body/clientes/index.jsx
+++ src/components/admin/body/clientes/index.jsx
@@ -4,6 +4,7 @@ const Clientes = () => {
 
     const componentsInThePage = [
         {title: 'Titulo', description: 'Titulo y descripción breve de la página', link: '/admin/slider', id: "frame1", url: '/images/admin/clients/title.png'},
+        {title: 'Clientes', description: 'Apartado donde se muestran los clientes que posee Reichstag', link: '/admin/about', id: "frame2", url: '/images/admin/clients/clients.png'},
         {title: 'Deslizador de clientes', description: 'Apartado donde se muestran los clientes que posee Reichstag', link: '/admin/about', id: "frame2", url: '/images/admin/clients/clients.png'},
     ];
 
