b31d53b96db994510c3cccce103587c36cb3c007
Julian Castro <141683762+Juliox14@users.noreply.github.com>
2024-09-02 23:27:14 -0600

    Fix: Correción de fetching de datos en items.js para que funcione en desarrollo

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@
 const nextConfig = {
     reactStrictMode: true,
     optimizeFonts: true,
-    eslint: {
-        ignoreDuringBuilds: true, // Desactiva ESLint durante la build
-    },
 };
 
 module.exports = nextConfig;
